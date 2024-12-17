97d84081473a891439ea50692011e6733ea6c367
garyuu09 <ribereo.minami@gmail.com>
2024-12-17 23:55:09 +0900

    Initial Commit

---
+++ TrackFit/TrackFitApp.swift
@@ -0,0 +1,32 @@
+//
+//  TrackFitApp.swift
+//  TrackFit
+//
+//  Created by Ryuga on 2024/12/17.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TrackFitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TrackFitTests/TrackFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackFitTests.swift
+//  TrackFitTests
+//
+//  Created by Ryuga on 2024/12/17.
+//
+
+import Testing
+@testable import TrackFit
+
+struct TrackFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
