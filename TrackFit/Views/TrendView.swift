3dd13395c15f94112501eee143af4400960588ef
garyuu09 <ribereo.minami@gmail.com>
2024-12-31 21:46:57 +0900

    fix: #4 AnalysisViewからTrendViewに変更。

---
+++ TrackFit/Views/TrendView.swift
@@ -0,0 +1,18 @@
+//
+//  TrendView.swift
+//  TrackFit
+//
+//  Created by Ryuga on 2024/12/31.
+//
+
+import SwiftUI
+
+struct TrendView: View {
+    var body: some View {
+        Text("TrendView")
+    }
+}
+
+#Preview {
+    TrendView()
+}
