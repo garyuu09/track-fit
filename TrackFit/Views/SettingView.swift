d8a00bd4cd90a33e4c08a723c46bfac23093c97c
garyuu09 <ribereo.minami@gmail.com>
2025-03-08 14:36:51 +0900

    fix: #19 タブをトレーニング一覧と設定のみに修正。

---
+++ TrackFit/Views/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  TrackFit
+//
+//  Created by Ryuga on 2025/03/08.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("SettingView")
+    }
+}
+
+#Preview {
+    SettingView()
+}
