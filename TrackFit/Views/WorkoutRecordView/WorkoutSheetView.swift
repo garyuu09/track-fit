dcd583149e220e4aaf0c07aa6b734738ec93679b
garyuu09 <ribereo.minami@gmail.com>
2025-05-20 09:57:50 +0900

    fix: #49 modify custom back button to default button

--- TrackFit/Views/WorkoutRecordView/WorkoutSheetView.swift
+++ TrackFit/Views/WorkoutRecordView/WorkoutSheetView.swift
@@ -95,33 +95,7 @@ struct WorkoutSheetView: View {
         }
         .navigationTitle("トレーニング管理")
         .navigationBarTitleDisplayMode(.inline)
-        .navigationBarBackButtonHidden(true)  // デフォルトの戻るボタンを隠す
         .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: {
-                    // 同期開始を通知
-                    NotificationCenter.default.post(name: .didStartSyncingWorkout, object: daily.id)
-                    Task {
-                        // Googleカレンダーにイベント作成
-                        let success = await viewModel.createEvent(dailyWorkout: daily)
-                        if success {
-                            // モデルを更新し永続化
-                            daily.isSyncedToCalendar = true
-                            try? context.save()
-                        }
-                        // 同期完了を通知
-                        NotificationCenter.default.post(
-                            name: .didFinishSyncingWorkout, object: daily.id)
-                        // ビューを閉じる
-                        dismiss()
-                    }
-                }) {
-                    HStack {
-                        Image(systemName: "chevron.left")
-                        Text("Back")
-                    }
-                }
-            }
             ToolbarItem(placement: .navigationBarTrailing) {
                 Button("保存") {
                     NotificationCenter.default.post(name: .didStartSyncingWorkout, object: daily.id)
